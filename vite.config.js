b942184e006d03f2d4fbcdb0ad36511f0be858d7
Ravindra Nath Jha <ravindranathjha76@gmail.com>
2025-04-23 01:13:48 +0530

    Fix Firebase build issues by updating Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Make sure Firebase modules are properly bundled
+      external: []
+    }
+  },
+  optimizeDeps: {
+    include: ['firebase/app', 'firebase/auth']
+  }
 })
